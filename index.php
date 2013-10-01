143cbc28fc766bea1cad3c2e0ca6b37bd3330598
martianmartin <martinmolina147@gmail.com>
2013-10-01 12:31:47 -0400

    the beggining of our gluten free tribe!

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+            <div class="stream" id="photoStream"></div>
+            <div class="stream" id="videoStream"></div>
+            <div class="stream" id="articleStream"></div>
+
+    </body>
+</html>
